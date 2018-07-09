d7f2176bf047a3a43152553a68c125d10c4144c0
James Kratus <jimkratus@gmail.com>
2018-07-09 13:00:33 -0400

    added redirect from danger, and formatted results page

--- dojo_survey/dojo_survey.py
+++ dojo_survey/dojo_survey.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,6 +14,11 @@ def create_user():
         'comment': request.form['comment']
     }
     return render_template("result.html", info = info)
+
+@app.route("/danger")
+def reroute_from_danger():
+    print("a user tried to visit /danger")
+    return redirect("/")
 
 if __name__=="__main__":
     app.run(debug=True)  

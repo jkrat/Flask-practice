f43f629a869178eec11cac7430a6563a53ed80e4
James Kratus <jimkratus@gmail.com>
2018-07-09 12:35:59 -0400

    almost done

--- dojo_survey/dojo_survey.py
+++ dojo_survey/dojo_survey.py
@@ -1,1 +1,19 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
 
+@app.route("/")
+def user_form():   
+    return render_template("index.html")
+
+@app.route("/result", methods=["POST"])
+def create_user():
+    info = {
+        'name': request.form['name'],
+        'location': request.form['location'],
+        'language': request.form['language'],
+        'comment': request.form['comment']
+    }
+    return render_template("result.html", info = info)
+
+if __name__=="__main__":
+    app.run(debug=True)  

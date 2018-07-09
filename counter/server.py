c3b228e7e5009063b760edc0152f22271852caa7
James Kratus <jimkratus@gmail.com>
2018-07-09 15:45:58 -0400

    visit counter created using sessions in flask

---
+++ counter/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)  
+app.secret_key = 'dortmund'
+app.count = 0
+
+@app.route('/')           
+def index():
+    if 'count' in session:
+        session['count'] += 1
+    else: 
+        session['count'] = 1
+    return render_template("index.html", count = session['count'])
+
+@app.route('/increment')
+def by_two():
+    session['count'] += 1
+    return redirect('/')
+
+@app.route('/reset')
+def reset():
+    session['count'] = 0
+    return redirect('/')
+
+
+
+
+if __name__=="__main__":   
+    app.run(debug=True)      

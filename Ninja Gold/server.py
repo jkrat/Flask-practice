40dafecd61f8d5d7a20dc0853f0ba71bc9235868
James Kratus <jimkratus@gmail.com>
2018-07-10 14:17:48 -0400

    added validation to survey

--- Ninja Gold/server.py
+++ Ninja Gold/server.py
@@ -5,15 +5,29 @@ import datetime
 app = Flask(__name__)  
 app.secret_key = 'dortmund'
 
-def find_luck():
-    luck = random.random()
-    return luck
+def random_num(a, b):
+    rnumber = random.randrange(a, b)
+    return rnumber
 
 def decide_win_or_lose():
     chances = random.randint(0, 1)
-        return chances == 1
-
-
+    return chances == 1
+    
+def add_activity(amount, action, property):
+    timestamp = datetime.datetime.now()
+    if property == 'Farm':
+        session['activity'].append(['earn', f'Earned {amount} golds from the {property}! ({timestamp})'])
+    if property == 'Cave':
+        session['activity'].append(['earn', f'Earned {amount} golds from the {property}! ({timestamp})'])  
+    if property == 'House':
+        session['activity'].append(['earn', f'Earned {amount} golds from the {property}! ({timestamp})'])
+    elif property == 'Casino':
+        if action == 'earned':
+            earned = f'Entered a casino and earned {amount} golds! ({timestamp})'
+            session['activity'].append(['earn', earned])
+        elif action == 'lost':
+            lost = f'Entered a casino and lost {amount} golds... Ouch. ({timestamp})' 
+            session['activity'].append(['lost', lost])
 
 @app.route('/')           
 def index():
@@ -21,29 +35,40 @@ def index():
         session['gold'] = 0
     if 'activity' not in session:
         session['activity'] = []
-    return render_template("index.html", gold=session['gold'], activity=session['activity'])
+    print(session['gold'])
+    print(session['activity'])
+    return render_template("index.html", gold=session['gold'], activity_list=session['activity'])
 
 @app.route('/process_money', methods=["POST"])
 def calc_gold():
-    luck = random.random()
-    if request.form('property') == 'Farm':
-        session['gold'] += (luck * 10 + 10)
-    elif request.form('property') == 'Cave':
-        session['gold'] += (luck * 5 + 5)
-    elif request.form('property') == 'House':
-        session['gold'] += (luck * 3 + 2)
-    elif request.form('property') == 'Casino':
-        win_chance = randon.randint(0,1)
-        if win_chance == 0:
-            session['gold'] + (luck * 50)
-        else:
-            session['gold'] - (luck * 50)
-
+    property_name = request.form['property']
+    if property_name == 'Farm':
+        farm_number = random_num(10, 21)
+        session['gold'] += farm_number
+        add_activity(farm_number, 'earned', 'Farm')
+    elif property_name == 'Cave':
+        cave_number = random_num(5, 10)
+        session['gold'] += cave_number
+        add_activity(cave_number, 'earned', 'Cave')
+    elif property_name == 'House':
+        house_number = random_num(2, 5)
+        session['gold'] += house_number
+        add_activity(house_number, 'earned', 'House')
+    elif property_name == 'Casino':
+        casino_number = random_num(0, 50)
+        chance = decide_win_or_lose()
+        if chance == True:
+            session['gold'] += casino_number
+            add_activity(casino_number, 'earned', 'Casino')
+        elif chance == False:
+            session['gold'] -= casino_number
+            add_activity(casino_number, 'lost', 'Casino')
     return redirect('/')
 
-@app.route('/reset', methods=["GET"])
+@app.route('/reset')
 def reset():
-    session.pop('number')  #OR session['count] = 0 OR session.clear() 
+    session['gold'] = 0  #OR session['count'] = 0 OR session.clear() OR session.pop('')
+    session['activity'] = []
     return redirect('/')
 
 if __name__=="__main__":   
--- dojo_survey/dojo_survey.py
+++ dojo_survey/dojo_survey.py
@@ -1,19 +1,32 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, flash, render_template, request, redirect, session
+
 app = Flask(__name__)
+app.secret_key = 'dortmund'
 
 @app.route("/")
 def user_form():   
+    session['info'] = {}
     return render_template("index.html")
 
-@app.route("/result", methods=["POST"])
+@app.route("/resut")
+def info_page():     
+    return render_template("result.html", info=session['info'])
+
+@app.route("/submit", methods=["POST"])
 def create_user():
-    info = {
-        'name': request.form['name'],
-        'location': request.form['location'],
-        'language': request.form['language'],
-        'comment': request.form['comment']
-    }
-    return render_template("result.html", info = info)
+    session['info'] = request.form
+
+    if len(request.form['name']) < 1:
+        flash('A name must be provided')
+    if len(request.form['comment']) < 1:
+        flash('A comment must be provided')
+    if len(request.form['comment']) > 120:
+        flash('A comment cannot have more than 120 characters')
+
+    if '_flashes' in session.keys():
+        return redirect('/')
+    else:
+        return render_template('result.html', info=session['info'])
 
 @app.route("/danger")
 def reroute_from_danger():
